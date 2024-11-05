a62a92bebaa2dd90a536d1700a64f6e1c9bd4988
DigbijayNayak555 <digbijay.nayak@peopletech.com>
2024-11-05 14:55:33 +0530

    processing javascript with babel cont..

--- gulpfile.js
+++ gulpfile.js
@@ -24,19 +24,13 @@ function css(cb) {
 }
 
 function js(cb) {
-  src([
-    `${origin}/js/lib/bootstrap.bundle.min.js`,
-    `${origin}/js/lib/fontawesome-all.min.js`,
-    `${origin}/js/lib/jquery.min.js`,
-    `${origin}/js/script.js`,
-  ])
+  src(`${origin}/js/lib/**/*.js`).pipe(dest(`${destination}/js/lib`));
+  src(`${origin}/js/script.js`)
     .pipe(
       babel({
-        compact: false,
         presets: ['@babel/env'],
       })
     )
-    .pipe(concatenate('build.js'))
     .pipe(dest(`${destination}/js`));
   cb();
 }
